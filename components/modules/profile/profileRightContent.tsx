d77f633d1350981b278e176c7117c2455c27d139
Dipankar Halder <dipankarhalder.dev@gmail.com>
2024-08-23 23:28:56 +0530

    updated code

---
+++ components/modules/profile/profileRightContent.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const ProfileRightContent = () => {
+  return (
+    <div className="w-1/4">
+      <div className="p-4">
+        <p>Activate Premium: 50% off</p>
+      </div>
+    </div>
+  );
+};
